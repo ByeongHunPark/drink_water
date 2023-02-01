1922ab37d647bf57365b77089502b41be4080477
ByeongHunPark <qkrqudgns9636@gmail.com>
2023-02-01 15:23:52 +0900

    start

--- drinkWater_uiKit/ViewController.swift
+++ drinkWater_uiKit/ViewController.swift
@@ -12,6 +12,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
+        
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        print("start")
     }
 
 
